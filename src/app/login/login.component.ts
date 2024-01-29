5a810d587ab341f18912f05659cb797559059bf9
ZattoniMatt2003 <118274997+ZattoniMatt2003@users.noreply.github.com>
2024-01-29 11:41:42 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import {RouterOutlet, Route, Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import UserModel from '../../models/UserModel';
-import { StoreServiceService } from '../store-service.service';
+import { StoreServiceService } from '../store.service';
 
 @Component({
   selector: 'login',
@@ -17,12 +17,12 @@ export class LoginComponent {
 
   readonly apiService: ApiService;
   readonly router: Router;
-  readonly storeService: StoreServiceService;
+  readonly storeService: StoreService;
 
   username: string = "";
   password: string = "";
 
-  constructor(apiService: ApiService, router: Router, storeService: StoreServiceService){
+  constructor(apiService: ApiService, router: Router, storeService: StoreService){
     this.apiService = apiService;
     this.router = router;
     this.storeService = storeService;
