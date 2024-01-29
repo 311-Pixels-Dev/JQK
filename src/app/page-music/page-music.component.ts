79480b528b868da70e893c36a8f9a71f94011070
Marco <marcoferrarese03@gmail.com>
2024-01-29 10:58:36 +0100

    Addes components

---
+++ src/app/page-music/page-music.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-music',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-music.component.html',
+  styleUrl: './page-music.component.css'
+})
+export class PageMusicComponent {
+
+}
---
+++ src/app/playlist-user/playlist-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlist-user.component.html',
+  styleUrl: './playlist-user.component.css'
+})
+export class PlaylistUserComponent {
+
+}
