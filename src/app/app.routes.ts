1ffff4005f1aa6621d82a33b1d609713ed56b294
Marco <marcoferrarese03@gmail.com>
2024-01-29 11:35:11 +0100

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PageMusicComponent } from './page-music/page-music.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "music", component: PageMusicComponent
+    },
+    {
+        path: "login", component: LoginComponent
+    }
+];
