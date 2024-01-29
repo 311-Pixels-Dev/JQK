004e9328d239b2b29fea71c8e5461cebb5e53867
ZattoniMatt2003 <118274997+ZattoniMatt2003@users.noreply.github.com>
2024-01-29 11:40:47 +0100

    Update store.service.ts

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import UserModel from '../models/UserModel';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StoreService {
+export class StoreServiceService {
 
   constructor() { }
+
+  saveUser(user: UserModel) {
+    localStorage.setItem('userid', user.id);
+    localStorage.setItem('username', user.username);
+  }
 }
