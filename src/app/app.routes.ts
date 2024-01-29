6ed416c4bfa7f03837cc611bbc58dd4b621eae35
Marco <marcoferrarese03@gmail.com>
2024-01-29 12:07:23 +0100

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PageMusicComponent } from './page-music/page-music.component';
 import { LoginComponent } from './login/login.component';
+import { PlaylistUserComponent } from './playlist-user/playlist-user.component';
 
 export const routes: Routes = [
     {
@@ -8,5 +9,8 @@ export const routes: Routes = [
     },
     {
         path: "login", component: LoginComponent
+    },
+    {
+        path: "playlists", component: PlaylistUserComponent
     }
 ];
