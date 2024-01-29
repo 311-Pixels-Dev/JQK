4e776757adf46de48e407a7a777880297c7ae256
ZattoniMatt2003 <118274997+ZattoniMatt2003@users.noreply.github.com>
2024-01-29 11:38:57 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {RouterOutlet, Route, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import UserModel from '../../models/UserModel';
+import { StoreServiceService } from '../store-service.service';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  readonly apiService: ApiService;
+  readonly router: Router;
+  readonly storeService: StoreServiceService;
+
+  username: string = "";
+  password: string = "";
+
+  constructor(apiService: ApiService, router: Router, storeService: StoreServiceService){
+    this.apiService = apiService;
+    this.router = router;
+    this.storeService = storeService;
+  }
+
+  async login() {
+    let user: UserModel = await this.apiService.loginUser(this.username, this.password);
+    console.log(user);
+    this.storeService.saveUser(user);
+    this.router.navigate(['final'])
+  }
 }
